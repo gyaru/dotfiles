1b53ce2629b1acdc8228c2fde9edab202db70481
gyaru <gyaru@users.noreply.github.com>
2026-08-30 23:55:25 +0200

    fix(seerr): add external IP and IP block for service and network policy

--- hosts/lapi/services/k3s.nix
+++ hosts/lapi/services/k3s.nix
@@ -23,6 +23,7 @@
       iptables -A nixos-fw -p tcp --dport 443 -s 192.168.1.0/24 -j nixos-fw-accept
       iptables -A nixos-fw -p tcp --dport 6443 -s 192.168.1.0/24 -j nixos-fw-accept
       iptables -A nixos-fw -p tcp --dport 8096 -s 192.168.1.0/24 -j nixos-fw-accept
+      iptables -A nixos-fw -p tcp --dport 5055 -s 192.168.1.0/24 -j nixos-fw-accept
     '';
   };
 
